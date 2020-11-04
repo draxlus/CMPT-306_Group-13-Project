c1da26b3de5e677331acfe53e44cd3d42e6c78fd
DUCSalatin <72632267+DUCSalatin@users.noreply.github.com>
2020-11-03 23:23:02 -0600

    Add files via upload

---
+++ Assets/obj/treestate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treestate : MonoBehaviour
+{
+
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
