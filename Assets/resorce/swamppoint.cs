0b0d97324ce6ae4d15cfcfe0f8e81babbfa88e36
DUCSalatin <72632267+DUCSalatin@users.noreply.github.com>
2020-11-04 19:11:14 -0600

    rename obj

---
+++ Assets/resorce/swamppoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swamppoint : MonoBehaviour
+{
+    public int health;
+    public int swampnumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1000;
+        swampnumber = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
